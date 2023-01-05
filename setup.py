2e2b0d8ea326d17488e83756b1ad8631a7f4aa8b
Rafael Goncalves Figueira <34547094+rafaelgfig@users.noreply.github.com>
2023-01-05 11:59:15 -0300

    Criando arquivos para biblioteca pública

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rafafun',
+    version='1.0',
+    license='MIT',
+    author="Rafael G. Figueira",
+    author_email='rafaelgfigueira@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/rafaelgfig/rafafun',
+    keywords='rafafun',
+    install_requires=[
+          'pandas',
+		  'numpy',
+		  'datetime',
+		  'zipfile'
+      ],
+
+)
